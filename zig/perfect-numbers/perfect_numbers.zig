56bea482bf6842ce01752992a80622f48fefcff9
Michael Allan Jackson <m@j5n.dev>
2024-06-12 19:11:39 -0400

    Add solution to perfect numbers

--- zig/perfect-numbers/perfect_numbers.zig
+++ zig/perfect-numbers/perfect_numbers.zig
@@ -6,6 +6,15 @@ pub const Classification = enum {
 
 /// Asserts that `n` is nonzero.
 pub fn classify(n: u64) Classification {
-    _ = n;
-    @compileError("please implement the classify function");
+    var sum: u64 = 0;
+    for (1..n) |i| {
+        if (n % i == 0) sum += i;
+    }
+    if (n < sum) {
+        return Classification.abundant;
+    } else  if (n == sum) {
+        return Classification.perfect;
+    } else {
+        return Classification.deficient;
+    }
 }
