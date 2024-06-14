0c4a53dcb3d32b6fc8823e7e8c7161c596f58c59
Michael Allan Jackson <m@j5n.dev>
2024-06-14 06:02:38 -0400

    Fix grains

--- zig/grains/grains.zig
+++ zig/grains/grains.zig
@@ -1,23 +1,14 @@
+const std = @import("std");
+
 pub const ChessboardError = error{
     IndexOutOfBounds,
 };
 
-pub fn board() [64]usize {
-    var b = [_]usize {0}**64;
-    b[0] = 1;
-    for (1..64) |i| b[i] = 2 * b[i-1];
-    return b;
-}
-
-const squares = board();
-
 pub fn square(index: usize) ChessboardError!u64 {
-    if (index > squares.len or index < 1 ) return ChessboardError.IndexOutOfBounds;
-    return squares[index-1];
+    if (index > 64 or index < 1 ) return ChessboardError.IndexOutOfBounds;
+    return std.math.powi(u64, 2, index - 1) catch unreachable;
 }
 
 pub fn total() u64 {
-    var sum: u64 = 0;
-    for (squares) |v| sum += v;
-    return sum;
+     return std.math.maxInt(u64);
 }
