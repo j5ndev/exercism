2a00d613998685996d029dab25b533371fbdb10f
Michael Allan Jackson <m@j5n.dev>
2024-06-14 08:56:06 -0400

    Use bitwise operators for grains

--- zig/grains/grains.zig
+++ zig/grains/grains.zig
@@ -6,9 +6,9 @@ pub const ChessboardError = error{
 
 pub fn square(index: usize) ChessboardError!u64 {
     if (index > 64 or index < 1 ) return ChessboardError.IndexOutOfBounds;
-    return std.math.powi(u64, 2, index - 1) catch unreachable;
+    return  @as(u64, 1) << @intCast(index - 1);
 }
 
 pub fn total() u64 {
-     return std.math.maxInt(u64);
+     return ~@as(u64, 0);
 }
