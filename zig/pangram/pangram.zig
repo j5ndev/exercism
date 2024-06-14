a037a19fdd1e3f4a7dcfa7533394eb290a1144f4
Michael Allan Jackson <m@j5n.dev>
2024-06-14 09:49:30 -0400

    Experiments with pangram

--- zig/pangram/pangram.zig
+++ zig/pangram/pangram.zig
@@ -1,11 +1,11 @@
 const ascii = @import("std").ascii;
 
 pub fn isPangram(str: []const u8) bool {
-    var r = [_]u8{0} ** 26;
+    var letters = [_]u1{0} ** 26;
     for (str) |l| {
         if (!ascii.isAlphabetic(l)) continue;
-        r[ascii.toUpper(l) - 'A'] = 1;
+        letters[ascii.toUpper(l) - 'A'] = 1;
     }
-    for (r) |v| if (v == 0) return false;
+    for (letters) |v| if (v == 0) return false;
     return true;
 }
