ae8664b72c96089c2ce101927181e4f6f26921fb
Michael Allan Jackson <m@j5n.dev>
2024-06-14 04:52:43 -0400

    Fix indexing issue

--- zig/reverse-string/reverse_string.zig
+++ zig/reverse-string/reverse_string.zig
@@ -1,7 +1,7 @@
 /// Writes a reversed copy of `s` to `buffer`.
 pub fn reverse(buffer: []u8, s: []const u8) []u8 {
-    for (s, 0..) |c, i| {
-        buffer[s.len-(1+i)] = c;
+    for (s, 1..) |c, i| {
+        buffer[s.len-i] = c;
     }
     return buffer;
 }
