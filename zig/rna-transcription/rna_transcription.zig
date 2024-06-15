887111ddb1bbbef43731e1def110f71f2fff31e7
Michael Allan Jackson <m@j5n.dev>
2024-06-15 17:38:21 -0400

    Change formatting of rna transcription

--- zig/rna-transcription/rna_transcription.zig
+++ zig/rna-transcription/rna_transcription.zig
@@ -2,11 +2,14 @@ const mem = @import("std").mem;
 
 pub fn toRna(allocator: mem.Allocator, dna: []const u8) mem.Allocator.Error![]const u8 {
     const rna = try allocator.alloc(u8, dna.len);
-    for (dna, 0..) |d,i| rna[i] = switch (d) {
-                                    'G' => 'C',
-                                    'C' => 'G',
-                                    'T' => 'A',
-                                    'A' => 'U',
-                                    else => continue,};
+    for (dna, 0..) |d,i| {
+        rna[i] = switch (d) {
+            'G' => 'C',
+            'C' => 'G',
+            'T' => 'A',
+            'A' => 'U',
+            else => continue,
+        };
+    }
     return rna;
 }
