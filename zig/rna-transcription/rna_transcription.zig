82a4fa1be415cf2ddd05ff7420c46adab46e93c4
Michael Allan Jackson <m@j5n.dev>
2024-06-15 17:35:25 -0400

    Add solution to rna transcription

--- zig/rna-transcription/rna_transcription.zig
+++ zig/rna-transcription/rna_transcription.zig
@@ -1,7 +1,12 @@
-// Import the appropriate standard library and modules
+const mem = @import("std").mem;
 
 pub fn toRna(allocator: mem.Allocator, dna: []const u8) mem.Allocator.Error![]const u8 {
-    _ = allocator;
-    _ = dna;
-    @compileError("please implement the toRna function");
+    const rna = try allocator.alloc(u8, dna.len);
+    for (dna, 0..) |d,i| rna[i] = switch (d) {
+                                    'G' => 'C',
+                                    'C' => 'G',
+                                    'T' => 'A',
+                                    'A' => 'U',
+                                    else => continue,};
+    return rna;
 }
