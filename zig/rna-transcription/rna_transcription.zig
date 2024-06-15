722d6f62dc4fe74648398a6dc49e8c1222ee7758
Michael Allan Jackson <m@j5n.dev>
2024-06-15 18:09:18 -0400

    Alter syntax for rna transcription

--- zig/rna-transcription/rna_transcription.zig
+++ zig/rna-transcription/rna_transcription.zig
@@ -1,9 +1,9 @@
 const mem = @import("std").mem;
 
 pub fn toRna(allocator: mem.Allocator, dna: []const u8) mem.Allocator.Error![]const u8 {
-    const rna = try allocator.alloc(u8, dna.len);
-    for (dna, 0..) |d,i| {
-        rna[i] = switch (d) {
+    const rna:[]u8 = try allocator.alloc(u8, dna.len);
+    for (dna, rna) |d,*r| {
+         r.* = switch (d) {
             'G' => 'C',
             'C' => 'G',
             'T' => 'A',
