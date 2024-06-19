5b2afd7d943b9e3a32f650a210d7ff4afacb729d
Michael Allan Jackson <m@j5n.dev>
2024-06-18 20:15:58 -0400

    Eliminate char counting for atbash cipher

--- zig/atbash-cipher/atbash_cipher.zig
+++ zig/atbash-cipher/atbash_cipher.zig
@@ -2,24 +2,9 @@ const std = @import("std");
 const mem = std.mem;
 const BLOCK_SIZE: usize = 5;
 
-fn countChar(s: []const u8) usize {
-    var count: usize = 0;
-    for (s) |c| {
-        switch (c) {
-            'a'...'z', 'A'...'Z', '0'...'9' => count += 1,
-            else => continue,
-        }
-    }
-    return count;
-}
-
 /// Encodes `s` using the Atbash cipher. Caller owns the returned memory.
 pub fn encode(allocator: mem.Allocator, s: []const u8) mem.Allocator.Error![]u8 {
-    const charCount = countChar(s);
-    if (charCount == 0) return try allocator.alloc(u8, 0);
-
-    const length: usize = charCount + if (charCount % 5 == 0) charCount/BLOCK_SIZE - 1 else charCount/BLOCK_SIZE;
-    const out: []u8 = try allocator.alloc(u8, length);
+    var out: []u8 = try allocator.alloc(u8, s.len + s.len / BLOCK_SIZE);
     var index: usize = 0;
     var blockCount: usize = 0;
     for (s) |c| {
@@ -38,12 +23,14 @@ pub fn encode(allocator: mem.Allocator, s: []const u8) mem.Allocator.Error![]u8 
         index += 1;
         blockCount += 1;
     }
+    _ = allocator.resize(out, index);
+     out.len = index;
     return out;
 }
 
 /// Decodes `s` using the Atbash cipher. Caller owns the returned memory.
 pub fn decode(allocator: mem.Allocator, s: []const u8) mem.Allocator.Error![]u8 {
-    const out: [] u8 = try allocator.alloc(u8, countChar(s));
+    var out: [] u8 = try allocator.alloc(u8, s.len);
     var index: usize = 0;
     for (s) |c| {
         const decoded = switch (c) {
@@ -54,5 +41,7 @@ pub fn decode(allocator: mem.Allocator, s: []const u8) mem.Allocator.Error![]u8 
         out[index] = decoded;
         index += 1;
     }
+     _ = allocator.resize(out, index);
+     out.len = index;
     return out;
 }
