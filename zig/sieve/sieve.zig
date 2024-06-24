b4cdb1102ea3e99e68e7491b065f9cf1304d2f00
Michael Allan Jackson <m@j5n.dev>
2024-06-23 20:59:03 -0400

    Modify sieve to use comptime limit

--- zig/sieve/sieve.zig
+++ zig/sieve/sieve.zig
@@ -1,10 +1,7 @@
 const std = @import("std");
-const SET_MAX: u32 = 1001;
 
-pub fn primes(buffer: []u32, limit: u32) []u32 {
-    std.debug.assert(limit < SET_MAX);
-    var set = std.bit_set.ArrayBitSet(u32, SET_MAX).initEmpty();
-
+pub fn primes(buffer: []u32, comptime limit: u32) []u32 {
+    var set = std.bit_set.ArrayBitSet(u32, limit + 1).initEmpty();
     var buffer_index: u32 = 0;
     var number: u32 = 2;
     while (number <= limit) : (number += 1) {
