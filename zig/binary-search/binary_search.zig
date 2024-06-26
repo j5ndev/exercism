3b128c611f046fc4514b8813f77a290c0cc94c00
Michael Allan Jackson <m@j5n.dev>
2024-06-26 15:47:07 -0400

    Improve variable names for binary search

--- zig/binary-search/binary_search.zig
+++ zig/binary-search/binary_search.zig
@@ -1,14 +1,14 @@
 pub fn binarySearch(comptime T: type, target: usize, items: []const T) ?usize {
-    var l: usize = 0;
-    var r: usize = items.len;
-    while (l < r) {
-        const m = (l + r) / 2;
-        if (target > items[m]) {
-            l = m + 1;
-        } else if (target < items[m]) {
-            r = m;
+    var left: usize = 0;
+    var right: usize = items.len;
+    while (left < right) {
+        const middle = (left + right) / 2;
+        if (target > items[middle]) {
+            left = middle + 1;
+        } else if (target < items[middle]) {
+            right = middle;
         } else {
-            return m;
+            return middle;
         }
     }
     return null;
