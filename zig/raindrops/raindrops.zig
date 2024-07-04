2f8e325911e31c08754331c7c1868a65bc91eb7a
Michael Allan Jackson <m@j5n.dev>
2024-07-04 09:33:25 -0400

    Use std library to compress code

--- zig/raindrops/raindrops.zig
+++ zig/raindrops/raindrops.zig
@@ -1,31 +1,11 @@
+const fixedBufferStream = @import("std").io.fixedBufferStream;
+
 pub fn convert(buffer: []u8, n: u32) []const u8 {
-    var index: usize = 0;
-    if (n % 3 == 0) index = writeToBuffer(buffer, "Pling", index);
-    if (n % 5 == 0) index = writeToBuffer(buffer, "Plang", index);
-    if (n % 7 == 0) index = writeToBuffer(buffer, "Plong", index);
-
-    // Write number to buffer if nothing was written
-    if (index == 0) {
-        var num: u32 = n;
-        while (num != 0) {
-            // Shift already written digits to the right
-            for (0..index) |i| {
-                buffer[index - i] = buffer[index - i - 1];
-            }
-            // Write next digit
-            buffer[0] = '0' + @as(u8, @intCast(num % 10));
-            index += 1;
-            num = num / 10;
-        }
-    }
-    return buffer[0..index];
-}
-
-fn writeToBuffer(buffer: []u8, s: []const u8, index: usize) usize {
-    var i: usize  = index;
-    for (s) |c| {
-        buffer[i] = c;
-        i += 1;
-    }
-    return i;
+    var stream = fixedBufferStream(buffer);
+    var writer = stream.writer();
+    if (n % 3 == 0) writer.writeAll("Pling") catch unreachable;
+    if (n % 5 == 0) writer.writeAll("Plang") catch unreachable;
+    if (n % 7 == 0) writer.writeAll("Plong") catch unreachable;
+    if (stream.pos == 0) writer.print("{}", .{n}) catch unreachable;
+    return stream.getWritten();
 }
