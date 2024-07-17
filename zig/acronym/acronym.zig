12e2458eca939dc10ff44932726eb65483d2a902
Michael Allan Jackson <m@j5n.dev>
2024-07-16 20:05:38 -0400

    Add solution to acronym

--- zig/acronym/acronym.zig
+++ zig/acronym/acronym.zig
@@ -2,7 +2,16 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn abbreviate(allocator: mem.Allocator, words: []const u8) mem.Allocator.Error![]u8 {
-    _ = allocator;
-    _ = words;
-    @compileError("please implement the abbreviate function");
+    var buffer = std.ArrayList(u8).init(allocator);
+    var next:bool = true;
+    for (words) |c| {
+        switch(c) {
+            ' ', '-', '_' => next = true,
+            else => if (next) {
+                        try buffer.append(std.ascii.toUpper(c));
+                        next = false;
+                    }
+        }
+    }
+    return buffer.toOwnedSlice();
 }
