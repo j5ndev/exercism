81c16c10554a52fb4c2764d4fb83a1ce9bca8184
Michael Allan Jackson <m@j5n.dev>
2024-07-20 18:58:19 -0400

    Add the solution to proverb

--- zig/proverb/proverb.zig
+++ zig/proverb/proverb.zig
@@ -1,5 +1,14 @@
+const std = @import("std");
+const mem = std.mem;
+const fmt = std.fmt;
+
 pub fn recite(allocator: mem.Allocator, words: []const []const u8) (fmt.AllocPrintError || mem.Allocator.Error)![][]u8 {
-    _ = allocator;
-    _ = words;
-    @compileError("please implement the recite function");
+    var result = try allocator.alloc([]u8, words.len);
+    errdefer allocator.free(result);
+    if (words.len == 0) return result;
+    for (words[0..words.len-1], 0..) |word, i| {
+        result[i] = try fmt.allocPrint(allocator, "For want of a {s} the {s} was lost.\n", .{word, words[i+1]});
+    }
+    result[words.len - 1] = try fmt.allocPrint(allocator, "And all for the want of a {s}.\n", .{words[0]});
+    return result;
 }
