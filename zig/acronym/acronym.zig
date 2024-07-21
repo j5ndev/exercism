86074c1a6eba0e2cddd87d523a37a352f3deb8d8
Michael Allan Jackson <m@j5n.dev>
2024-07-20 20:31:36 -0400

    Add errdefer to the acronym solution

--- zig/acronym/acronym.zig
+++ zig/acronym/acronym.zig
@@ -3,6 +3,7 @@ const mem = std.mem;
 
 pub fn abbreviate(allocator: mem.Allocator, words: []const u8) mem.Allocator.Error![]u8 {
     var buffer = std.ArrayList(u8).init(allocator);
+    errdefer buffer.deinit();
     var next:bool = true;
     for (words) |c| {
         switch(c) {
