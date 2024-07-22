caef3b3babd984ad0660c412329004cd7c2cd06c
Michael Allan Jackson <m@j5n.dev>
2024-07-22 10:54:37 -0400

    Change to numbers to binary notation in secret-handshake

--- zig/secret-handshake/secret_handshake.zig
+++ zig/secret-handshake/secret_handshake.zig
@@ -1,11 +1,11 @@
 const std = @import("std");
 const mem = std.mem;
 
-pub const Signal = enum(u4) {
-    wink = 1,
-    double_blink = 2,
-    close_your_eyes = 4,
-    jump = 8,
+pub const Signal = enum(u8) {
+    wink = 0b00001,
+    double_blink = 0b00010,
+    close_your_eyes = 0b00100,
+    jump = 0b01000,
 };
 
 pub fn calculateHandshake(allocator: mem.Allocator, number: u5) mem.Allocator.Error![]const Signal {
@@ -14,6 +14,6 @@ pub fn calculateHandshake(allocator: mem.Allocator, number: u5) mem.Allocator.Er
     inline for (std.meta.fields(Signal)) |f| {
         if (f.value & number != 0) try result.append(@enumFromInt(f.value));
     }
-    if (16 & number != 0) mem.reverse(Signal, result.items);
+    if (0b10000 & number != 0) mem.reverse(Signal, result.items);
     return result.toOwnedSlice();
 }
