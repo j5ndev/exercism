06106269a96a4cd71a2cf03ca75fa0ae96254710
Michael Allan Jackson <m@j5n.dev>
2024-07-22 10:29:00 -0400

    Add solution to allergies

--- zig/allergies/allergies.zig
+++ zig/allergies/allergies.zig
@@ -13,12 +13,18 @@ pub const Allergen = enum {
 };
 
 pub fn isAllergicTo(score: u8, allergen: Allergen) bool {
-    _ = score;
-    _ = allergen;
-    @compileError("please implement the isAllergicTo function");
+    return initAllergenSet(score).contains(allergen);
 }
 
 pub fn initAllergenSet(score: usize) EnumSet(Allergen) {
-    _ = score;
-    @compileError("please implement the initAllergenSet function");
+    var result = EnumSet(Allergen).init(.{});
+    if (0b00000001 & score != 0) result.insert(.eggs);
+    if (0b00000010 & score != 0) result.insert(.peanuts);
+    if (0b00000100 & score != 0) result.insert(.shellfish);
+    if (0b00001000 & score != 0) result.insert(.strawberries);
+    if (0b00010000 & score != 0) result.insert(.tomatoes);
+    if (0b00100000 & score != 0) result.insert(.chocolate);
+    if (0b01000000 & score != 0) result.insert(.pollen);
+    if (0b10000000 & score != 0) result.insert(.cats);
+    return result;
 }
