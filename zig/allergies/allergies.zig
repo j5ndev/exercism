451b6248514c3c56214de0c5f7b38fe84f1c69f2
Michael Allan Jackson <m@j5n.dev>
2024-07-22 10:40:36 -0400

    Compress solution for allergies

--- zig/allergies/allergies.zig
+++ zig/allergies/allergies.zig
@@ -1,15 +1,15 @@
 const std = @import("std");
 const EnumSet = std.EnumSet;
 
-pub const Allergen = enum {
-    eggs,
-    peanuts,
-    shellfish,
-    strawberries,
-    tomatoes,
-    chocolate,
-    pollen,
-    cats,
+pub const Allergen = enum(u8) {
+    eggs = 1,
+    peanuts = 2,
+    shellfish = 4,
+    strawberries = 8,
+    tomatoes = 16,
+    chocolate = 32,
+    pollen = 64,
+    cats = 128,
 };
 
 pub fn isAllergicTo(score: u8, allergen: Allergen) bool {
@@ -18,13 +18,8 @@ pub fn isAllergicTo(score: u8, allergen: Allergen) bool {
 
 pub fn initAllergenSet(score: usize) EnumSet(Allergen) {
     var result = EnumSet(Allergen).init(.{});
-    if (0b00000001 & score != 0) result.insert(.eggs);
-    if (0b00000010 & score != 0) result.insert(.peanuts);
-    if (0b00000100 & score != 0) result.insert(.shellfish);
-    if (0b00001000 & score != 0) result.insert(.strawberries);
-    if (0b00010000 & score != 0) result.insert(.tomatoes);
-    if (0b00100000 & score != 0) result.insert(.chocolate);
-    if (0b01000000 & score != 0) result.insert(.pollen);
-    if (0b10000000 & score != 0) result.insert(.cats);
+    inline for (std.meta.fields(Allergen)) |f|{
+        if (f.value & score != 0) result.insert(@enumFromInt(f.value));
+    }
     return result;
 }
