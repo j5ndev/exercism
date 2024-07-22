2890b1d5c89b7d1e3dfe80c40bcbee920218cf66
Michael Allan Jackson <m@j5n.dev>
2024-07-22 10:48:29 -0400

    Compress secret handshake

--- zig/secret-handshake/secret_handshake.zig
+++ zig/secret-handshake/secret_handshake.zig
@@ -1,22 +1,19 @@
 const std = @import("std");
 const mem = std.mem;
 
-pub const Signal = enum {
-    close_your_eyes,
-    double_blink,
-    jump,
-    wink,
+pub const Signal = enum(u4) {
+    wink = 1,
+    double_blink = 2,
+    close_your_eyes = 4,
+    jump = 8,
 };
 
 pub fn calculateHandshake(allocator: mem.Allocator, number: u5) mem.Allocator.Error![]const Signal {
     var result = std.ArrayList(Signal).init(allocator);
     errdefer result.deinit();
-
-    if (0b00001 & number != 0) try result.append(.wink);
-    if (0b00010 & number != 0) try result.append(.double_blink);
-    if (0b00100 & number != 0) try result.append(.close_your_eyes);
-    if (0b01000 & number != 0) try result.append(.jump);
-    if (0b10000 & number != 0) mem.reverse(Signal, result.items);
-
+    inline for (std.meta.fields(Signal)) |f| {
+        if (f.value & number != 0) try result.append(@enumFromInt(f.value));
+    }
+    if (16 & number != 0) mem.reverse(Signal, result.items);
     return result.toOwnedSlice();
 }
