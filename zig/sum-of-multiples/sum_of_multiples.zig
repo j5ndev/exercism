e1fc0e9d173444898468040e453f76d60ddb5c4a
Michael Allan Jackson <m@j5n.dev>
2024-07-29 16:54:03 -0400

    Add the solution for sum-of-multiples

--- zig/sum-of-multiples/sum_of_multiples.zig
+++ zig/sum-of-multiples/sum_of_multiples.zig
@@ -2,8 +2,14 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn sum(allocator: mem.Allocator, factors: []const u32, limit: u32) !u64 {
-    _ = allocator;
-    _ = factors;
-    _ = limit;
-    @compileError("please implement the sum function");
+    var multiples = try std.DynamicBitSet.initEmpty(allocator, limit);
+    defer multiples.deinit();
+    for (factors) |f| if (f != 0) {
+        var m = f;
+        while (m < limit) : (m += f) multiples.set(m);
+    };
+    var result: u64 = 0;
+    var iterator = multiples.iterator(.{});
+    while (iterator.next()) |m| result += m;
+    return result;
 }
