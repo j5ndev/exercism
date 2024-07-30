125c242a08214ce2149c28e75215e0dea8f25761
Michael Allan Jackson <m@j5n.dev>
2024-07-30 12:05:09 -0400

    Add the solution to anagram

--- zig/anagram/anagram.zig
+++ zig/anagram/anagram.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const mem = std.mem;
+const toLower = std.ascii.toLower;
 
 /// Returns the set of strings in `candidates` that are anagrams of `word`.
 /// Caller owns the returned memory.
@@ -8,8 +9,25 @@ pub fn detectAnagrams(
     word: []const u8,
     candidates: []const []const u8,
 ) !std.BufSet {
-    _ = allocator;
-    _ = word;
-    _ = candidates;
-    @compileError("please implement the detectAnagrams function");
+    var result = std.BufSet.init(allocator);
+    errdefer result.deinit();
+    const h = histogram(word);
+    for (candidates) |c| if (eq(h, histogram(c)) and !same(word, c)) try result.insert(c);
+    return result;
+}
+
+fn histogram(word: []const u8) [27]u8 {
+    var result = [_]u8 {0}**27;
+    for (word) |c| result[toLower(c) - 97] += 1;
+    return result;
+}
+
+fn eq(h1: [27] u8, h2: [27] u8) bool {
+    for (h1, h2) |v1, v2| if (v1 != v2) return false;
+    return true;
+}
+
+fn same(w1: []const u8, w2: []const u8) bool {
+    for (w1, w2) |c1, c2| if (toLower(c1) != toLower(c2)) return false;
+    return true;
 }
