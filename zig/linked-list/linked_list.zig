b0573bc566dea91159810e0cc948816ee29a49ef
Michael Allan Jackson <m@j5n.dev>
2024-07-30 20:14:21 -0400

    Add the solution to linked-list

--- zig/linked-list/linked_list.zig
+++ zig/linked-list/linked_list.zig
@@ -1,42 +1,88 @@
 pub fn LinkedList(comptime T: type) type {
     _ = T;
     return struct {
-        // Please implement the doubly linked `Node` (replacing each `void`).
+        const Self = @This();
         pub const Node = struct {
-            prev: void,
-            next: void,
-            data: void,
+            prev: ?*Node = null,
+            next: ?*Node = null,
+            data: ?usize = null,
         };
 
-        // Please implement the fields of the linked list (replacing each `void`).
-        first: void,
-        last: void,
-        len: void,
+        first: ?*Node = null,
+        last: ?*Node = null,
+        len: usize = 0,
 
-        // Please implement the below methods.
-        // You need to add the parameters to each method.
-
-        pub fn push() void {
-            // Please implement this method.
+        // Add node to the end of the list
+        pub fn push(self: *Self, node: *Node) void {
+            if (self.last) |n| {
+                n.next = node;
+                node.prev = n;
+            } else {
+                self.first = node;
+            }
+            self.last = node;
+            self.len += 1;
         }
 
-        pub fn pop() void {
-            // Please implement this method.
-            // It must return an optional pointer to a Node.
+        // Remove node from the end of the list
+        pub fn pop(self: *Self) ?*Node {
+            const result = self.last;
+            if (result) |n| {
+                self.last = n.prev;
+                if (self.first == result) self.first = null;
+                n.next = null;
+                n.prev = null;
+                self.len -= 1;
+            }
+            return result;
         }
 
-        pub fn shift() void {
-            // Please implement this method.
-            // It must return an optional pointer to a Node.
+        // Remove node from the start of the list
+        pub fn shift(self: *Self) ?*Node {
+            const result = self.first;
+            if (result) |n| {
+                self.first = n.next;
+                if (self.first == null) self.last = null;
+                n.prev = null;
+                n.next = null;
+                self.len -= 1;
+            }
+            return result;
         }
 
-        pub fn unshift() void {
-            // Please implement this method.
+        // Add node to the start of the list
+        pub fn unshift(self: *Self, node: *Node) void {
+            if (self.first) |n| {
+                node.next = n;
+                n.prev = node;
+            } else {
+                self.last = node;
+            }
+            self.first = node;
+            self.len += 1;
         }
 
-        pub fn delete() void {
-            // Please implement this method.
-            // It must modify the list only when it contains the given node.
+        // Remove node from anywhere in the list
+        pub fn delete(self: *Self, node: *Node) void {
+            var current: ?*Node = self.first;
+            while (current) |n| : (current = n.next) {
+                if (n == node) {
+                    if (n.prev) |p| {
+                        p.next = n.next;
+                    } else {
+                        self.first = n.next;
+                    }
+                    if (n.next) |q| {
+                        q.prev = n.prev;
+                    } else {
+                        self.last = n.prev;
+                    }
+                    n.next = null;
+                    n.prev = null;
+                    self.len -= 1;
+                    break;
+                }
+            }
         }
     };
 }
