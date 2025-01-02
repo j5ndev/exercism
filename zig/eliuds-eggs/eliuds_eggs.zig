308edef1d63a76e9374c0ed2f13c931c97819ae5
Michael Allan Jackson <m@j5n.dev>
2025-01-02 13:06:10 -0500

    Add the solution to zig/eliuds-eggs

--- zig/eliuds-eggs/eliuds_eggs.zig
+++ zig/eliuds-eggs/eliuds_eggs.zig
@@ -1,4 +1,9 @@
+const std = @import("std");
+
 pub fn eggCount(number: usize) usize {
-    _ = number;
-    @compileError("please implement the eggCount function");
+    var result: usize = 0;
+    for (0..@bitSizeOf(usize)) |i| {
+        result += (number & std.math.pow(usize, 2, i)) >> @truncate(i);
+    }
+    return result;
 }
