54b47921344155a4b4fe55e2eba2ee60b0118645
Michael Allan Jackson <m@j5n.dev>
2025-01-19 07:09:21 -0500

    Add the solution to zig/rotational-cipher

--- zig/rotational-cipher/rotational_cipher.zig
+++ zig/rotational-cipher/rotational_cipher.zig
@@ -1,6 +1,28 @@
+const std = @import("std");
+const mem = std.mem;
+const ascii = std.ascii;
+
+// Rotate text by shiftKey; caller owns the returned memory
 pub fn rotate(allocator: mem.Allocator, text: []const u8, shiftKey: u5) mem.Allocator.Error![]u8 {
-    _ = allocator;
-    _ = text;
-    _ = shiftKey;
-    @compileError("please implement the rotate function");
+    var result: []u8 = try allocator.alloc(u8, text.len);
+
+    if (shiftKey == 0 or shiftKey == 26) {
+        @memcpy(result, text);
+    } else {
+        for (text, 0..) |c, i| {
+            if (ascii.isAlphabetic(c)) {
+                var new: u8 = c + shiftKey;
+                if (ascii.isUpper(c)) {
+                    if (!ascii.isUpper(new)) new -= 26;
+                } else {
+                    if (!ascii.isLower(new)) new -= 26;
+                }
+                result[i] = new;
+            } else {
+                result[i] = c;
+            }
+        }
+    }
+
+    return result;
 }
