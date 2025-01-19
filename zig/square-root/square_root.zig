93c12a7011ffdf8ba2ed604d083d8181a3e795de
Michael Allan Jackson <m@j5n.dev>
2025-01-18 23:20:57 -0500

    Add the solution to zig/square-root

--- zig/square-root/square_root.zig
+++ zig/square-root/square_root.zig
@@ -1,4 +1,23 @@
+// Adapted from:
+//   https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Binary_numeral_system_(base_2)
 pub fn squareRoot(radicand: usize) usize {
-    _ = radicand;
-    @compileError("please implement the squareRoot function");
+    var x: usize = radicand;
+    var c: usize = 0;
+
+    // largest power of 4 in usize
+    var d: usize = 1 << @bitSizeOf(usize) - 2;
+
+    // largest power of four <= n
+    while (d > radicand) d >>= 2;
+
+    while (d != 0) {
+        if (x >= c + d) {
+            x -= c + d;
+            c = (c >> 1) + d;
+        } else {
+            c >>= 1;
+        }
+        d >>= 2;
+    }
+    return c;
 }
