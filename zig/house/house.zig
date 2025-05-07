707074671aa0e3f77dd2b40ed8828ac9e2b8b361
Michael Allan Jackson <m@j5n.dev>
2025-05-07 08:48:20 -0400

    Switch to using fixedBufferStream in zig/house

--- zig/house/house.zig
+++ zig/house/house.zig
@@ -1,16 +1,10 @@
-const bufPrint = @import("std").fmt.bufPrint;
-
-// Write to a slice
-// Returns the length of bytes that were written
-fn write(buffer: []u8, comptime fmt: []const u8, args: anytype) usize {
-    return (bufPrint(buffer, fmt, args) catch "").len;
-}
+const fixedBufferStream = @import("std").io.fixedBufferStream;
 
 pub fn recite(buffer: []u8, start_verse: u32, end_verse: u32) []const u8 {
     if (start_verse > end_verse or start_verse < 1) {
         return buffer;
     }
-    const verses = [_][2][]const u8{
+    const parts = [_][2][]const u8{
         .{"house", "Jack built."},
         .{"malt", "lay in"},
         .{"rat", "ate"},
@@ -24,20 +18,22 @@ pub fn recite(buffer: []u8, start_verse: u32, end_verse: u32) []const u8 {
         .{"farmer sowing his corn", "kept"},
         .{"horse and the hound and the horn", "belonged to"},
     };
-    var end: usize = 0;
+
+    var fbs = fixedBufferStream(buffer);
+    const fbsw = fbs.writer();
     for (start_verse - 1 .. end_verse) |i| {
 
         // Write a newline if this is not the first verse
-        if (end > 0) end += write(buffer[end..], "\n", .{});
+        if (fbs.pos > 0) fbsw.writeAll("\n") catch unreachable;
 
         // Write the start of the verse
-        end += write(buffer[end..], "This is", .{});
+       fbsw.writeAll("This is") catch unreachable;
     
         // Write each part of the verse
         for (0 .. i + 1) |j| {
-            const verse: [2][]const u8 = verses[i - j];
-            end += write(buffer[end..], " the {s} that {s}", .{ verse[0], verse[1] });
+            const part: [2][]const u8 = parts[i - j];
+            fbsw.print(" the {s} that {s}", .{ part[0], part[1] }) catch unreachable;
         }
     }
-    return buffer[0 .. end];
+    return buffer[0 .. fbs.pos];
 }
