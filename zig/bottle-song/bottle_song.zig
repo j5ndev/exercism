dd5f81e58542687c1ff65e79b1e31558e2897a49
Michael Allan Jackson <m@j5n.dev>
2025-05-08 21:35:03 -0400

    wip

--- zig/bottle-song/bottle_song.zig
+++ zig/bottle-song/bottle_song.zig
@@ -1,6 +1,52 @@
+const fixedBufferStream = @import("std").io.fixedBufferStream;
+
 pub fn recite(buffer: []u8, start_bottles: u32, take_down: u32) []const u8 {
-    _ = buffer;
-    _ = start_bottles;
-    _ = take_down;
-    @compileError("please implement the recite function");
+    var take = take_down;
+    var start = start_bottles;
+    if (start_bottles < take_down) {
+        take = start_bottles;
+    }
+    if (start_bottles > 10) {
+        start = 10;
+    }
+
+    const numbers = [_][] const u8 {
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+    };
+
+    var fbs = fixedBufferStream(buffer);
+    const fbsw = fbs.writer();
+    for (start - 1 .. start + take) |i| {
+        
+        // Write a newline if this is not the first verse
+        if (fbs.pos > 0) fbsw.writeAll("\n") catch unreachable;
+
+        // Write the verse
+        const format = 
+            \\{s} green bottles hanging on the wall
+            \\{s} green bottles hanging on the wall
+            \\And if one green bottle should accidentally fall,
+            \\There'll be {s} green bottle{s} hanging on the wall.
+        ;
+        const number = numbers[i];
+        var one_less = "no";
+        if (i > 0) {
+            one_less = numbers[i-1];
+        }
+        var part = "";
+        if (i == 1) {
+            part = "s";
+        }
+        //TODO: handle conditional capitalization
+       fbsw.print(format, .{number, number, one_less, part}) catch unreachable;
+    }
 }
