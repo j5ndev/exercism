38ace922f76a427965d2a8f44804e5c194e494f2
Michael Allan Jackson <m@j5n.dev>
2025-05-10 21:56:44 -0400

    Add the solution to zig/bottle_song

--- zig/bottle-song/bottle_song.zig
+++ zig/bottle-song/bottle_song.zig
@@ -2,15 +2,15 @@ const fixedBufferStream = @import("std").io.fixedBufferStream;
 const toLower = @import("std").ascii.toLower;
 pub fn recite(buffer: []u8, start_bottles: u32, take_down: u32) []const u8 {
     var take = take_down;
-    var start = start_bottles;
-    if (start_bottles < take_down) {
-        take = start_bottles;
-    }
+    var pos = start_bottles;
     if (start_bottles > 10) {
-        start = 10;
+        pos = 10;
     }
-
-    const numbers = [_][] const u8 {
+    if (pos - take < 0) {
+        take = pos;
+    }
+    const numbers = [_][]const u8{
+        "no",
         "One",
         "Two",
         "Three",
@@ -25,28 +25,30 @@ pub fn recite(buffer: []u8, start_bottles: u32, take_down: u32) []const u8 {
 
     var fbs = fixedBufferStream(buffer);
     const fbsw = fbs.writer();
-    for (start - 1 .. start + take) |i| {
-        
+    const stop: u32 = pos - take;
+    while (pos > stop) : (pos -= 1) {
+
         // Write a newline if this is not the first verse
-        if (fbs.pos > 0) fbsw.writeAll("\n") catch unreachable;
+        if (fbs.pos > 0) fbsw.writeAll("\n\n") catch unreachable;
 
         // Write the verse
-        const format = 
-            \\{s} green bottles hanging on the wall
-            \\{s} green bottles hanging on the wall
+        const format =
+            \\{s} green bottle{s} hanging on the wall,
+            \\{s} green bottle{s} hanging on the wall,
             \\And if one green bottle should accidentally fall,
             \\There'll be {c}{s} green bottle{s} hanging on the wall.
         ;
-        const number = numbers[i];
-        var one_less: []const u8 = "no";
-        if (i > 0) {
-            one_less = numbers[i-1];
+        const number = numbers[pos];
+        const one_less = numbers[pos - 1];
+        var one_less_part: []const u8 = "s";
+        if (pos == 2) {
+            one_less_part = "";
         }
-        var part: []const u8 = "";
-        if (i == 1) {
-            part = "s";
+        var number_part: []const u8 = "s";
+        if (pos == 1) {
+            number_part = "";
         }
-       fbsw.print(format, .{number, number, toLower(one_less[0]), one_less[1..], part}) catch unreachable;
+        fbsw.print(format, .{ number, number_part, number, number_part, toLower(one_less[0]), one_less[1..], one_less_part }) catch unreachable;
     }
-    return buffer[0 .. fbs.pos];
+    return buffer[0..fbs.pos];
 }
