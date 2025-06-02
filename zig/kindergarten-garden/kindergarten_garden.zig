7afbe9cedf4e21a43b98a9be7f855c0f028c9b11
Michael Allan Jackson <m@j5n.dev>
2025-06-01 22:14:54 -0400

    Add the solution to zig/kindergarten-garden

--- zig/kindergarten-garden/kindergarten_garden.zig
+++ zig/kindergarten-garden/kindergarten_garden.zig
@@ -5,8 +5,23 @@ pub const Plant = enum {
     violets,
 };
 
+fn plantFromCode(c: u8) Plant {
+    return switch (c) {
+        'C' => .clover,
+        'G' => .grass,
+        'R' => .radishes,
+        'V' => .violets,
+        else => unreachable,
+    };
+}
+
 pub fn plants(diagram: []const u8, student: []const u8) [4]Plant {
-    _ = diagram;
-    _ = student;
-    @compileError("please implement the plants function");
+    const indexRow1 = (student[0] - 'A') * 2;
+    const indexRow2 = indexRow1 + (diagram.len / 2 + 1);
+    return .{
+        plantFromCode(diagram[indexRow1]),
+        plantFromCode(diagram[indexRow1 + 1]),
+        plantFromCode(diagram[indexRow2]),
+        plantFromCode(diagram[indexRow2 + 1]),
+    };
 }
