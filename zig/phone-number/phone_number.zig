f1b2505e1553b4cb5cf6cb06ce06229d2d02807a
Michael Allan Jackson <m@j5n.dev>
2025-06-14 17:02:19 -0400

    Add the solution to zig/phone-number

--- zig/phone-number/phone_number.zig
+++ zig/phone-number/phone_number.zig
@@ -1,4 +1,18 @@
+const ascii = @import("std").ascii;
+
 pub fn clean(phrase: []const u8) ?[10]u8 {
-    _ = phrase;
-    @compileError("please implement the clean function");
+    var count: u32 = 0;
+    var result: [10]u8 = .{0} ** 10;
+    for (phrase) |c| {
+        if (ascii.isDigit(c)) {
+            if (count == 10) return null;
+            if (count == 0 and c == '1') continue;
+            if (count == 0 and c == '0') return null;
+            if (count == 3 and (c == '0' or c == '1')) return null;
+            result[count] = c;
+            count += 1;
+        }
+    }
+    if (count < 10) return null;
+    return result;
 }
